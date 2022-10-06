6648330481d8ba2f3ef261a2bc6ce16153c726cb
Moringa School <Abdulbasid.sheikh@student.moringaschool>
2022-10-06 15:34:31 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+document.querySelector(`main`).remove(); 
+
+const newHeader = document.createElement("h1")
+    newHeader.setAttribute("id","victory")
+        document.body.appendChild(newHeader)
+newHeader.innerHTML = "ABDUL is the champion"
